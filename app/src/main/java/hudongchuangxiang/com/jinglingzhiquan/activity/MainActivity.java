0c580e7c72077fccc1ebea38f0b2bea93c1c3032
ZhangJieBo <530092772@qq.com>
2017-08-29 16:48:25 +0800

    第一次保存2017年8月29日16:48:13

---
+++ app/src/main/java/hudongchuangxiang/com/jinglingzhiquan/activity/MainActivity.java
@@ -0,0 +1,66 @@
+package hudongchuangxiang.com.jinglingzhiquan.activity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import hudongchuangxiang.com.jinglingzhiquan.R;
+import hudongchuangxiang.com.jinglingzhiquan.fragment.ShengQianFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+    private String[] tabsItem = new String[5];
+    private Class[] fragment = new Class[]{
+            ShengQianFragment.class,
+            ShengQianFragment.class,
+            ShengQianFragment.class,
+            ShengQianFragment.class,
+            ShengQianFragment.class
+    };
+    private int[] imgRes = new int[]{
+            R.drawable.selector_shengqian_item,
+            R.drawable.selector_zhuanqian_item,
+            R.drawable.selector_shoukuan_item,
+            R.drawable.selector_renzheng_item,
+            R.drawable.selector_mine_item
+    };
+    private FragmentTabHost mTabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        //禁止横屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        tabsItem[0] = "省钱";
+        tabsItem[1] = "赚钱";
+        tabsItem[2] = "收款";
+        tabsItem[3] = "认证";
+        tabsItem[4] = "我的";
+
+        mTabHost = (FragmentTabHost) findViewById(R.id.tabHost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtab);
+        for (int i = 0; i < tabsItem.length; i++) {
+            View inflate = null;
+            inflate = getLayoutInflater().inflate(R.layout.tabs_item, null);
+            TextView tabs_text = (TextView) inflate.findViewById(R.id.tabs_text);
+            ImageView tabs_img = (ImageView) inflate.findViewById(R.id.tabs_img);
+            tabs_text.setText(tabsItem[i]);
+            tabs_img.setImageResource(imgRes[i]);
+            mTabHost.addTab(mTabHost.newTabSpec(tabsItem[i]).setIndicator(inflate), fragment[i], null);
+        }
+    }
+}
---
+++ app/src/main/java/hudongchuangxiang/com/jinglingzhiquan/fragment/ShengQianFragment.java
@@ -0,0 +1,30 @@
+package hudongchuangxiang.com.jinglingzhiquan.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hudongchuangxiang.com.jinglingzhiquan.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShengQianFragment extends Fragment {
+
+
+    public ShengQianFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sheng_qian, container, false);
+    }
+
+}
