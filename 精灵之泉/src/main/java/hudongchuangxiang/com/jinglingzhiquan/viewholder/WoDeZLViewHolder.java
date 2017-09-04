ce004317958152eae6c90108a10fead3383c4bf9
ZhangJieBo <530092772@qq.com>
2017-09-04 16:39:44 +0800

    地址管理

--- 精灵之泉/src/main/java/hudongchuangxiang/com/jinglingzhiquan/viewholder/WoDeZLViewHolder.java
+++ 精灵之泉/src/main/java/hudongchuangxiang/com/jinglingzhiquan/viewholder/WoDeZLViewHolder.java
@@ -1,9 +1,14 @@
 package hudongchuangxiang.com.jinglingzhiquan.viewholder;
 
+import android.content.Intent;
 import android.support.annotation.LayoutRes;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+
+import hudongchuangxiang.com.jinglingzhiquan.R;
+import hudongchuangxiang.com.jinglingzhiquan.activity.DiZhiGLActivity;
 
 /**
  * Created by Administrator on 2017/3/28 0028.
@@ -12,6 +17,14 @@ public class WoDeZLViewHolder extends BaseViewHolder<Integer> {
 
     public WoDeZLViewHolder(ViewGroup parent, @LayoutRes int res) {
         super(parent, res);
+        $(R.id.viewDiZhiGL).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getContext(), DiZhiGLActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
