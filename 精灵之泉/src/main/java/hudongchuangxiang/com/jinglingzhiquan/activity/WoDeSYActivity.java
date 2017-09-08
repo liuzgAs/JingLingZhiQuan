76d27cffb5df2e8fa122d65fa9682456bb52cc92
ZhangJieBo <530092772@qq.com>
2017-09-08 11:26:39 +0800

    我的收益界面重新制作

--- 精灵之泉/src/main/java/hudongchuangxiang/com/jinglingzhiquan/activity/WoDeSYActivity.java
+++ 精灵之泉/src/main/java/hudongchuangxiang/com/jinglingzhiquan/activity/WoDeSYActivity.java
@@ -2,6 +2,9 @@ package hudongchuangxiang.com.jinglingzhiquan.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,6 +16,9 @@ import hudongchuangxiang.com.jinglingzhiquan.util.ScreenUtils;
 public class WoDeSYActivity extends ZjbBaseActivity implements View.OnClickListener {
 
     private View viewBar;
+    private TextView textFenRun;
+    private TextView textYongJin;
+    private TextView textFanYong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,9 @@ public class WoDeSYActivity extends ZjbBaseActivity implements View.OnClickListe
     @Override
     protected void findID() {
         viewBar = findViewById(R.id.viewBar);
+        textFenRun = (TextView) findViewById(R.id.textFenRun);
+        textYongJin = (TextView) findViewById(R.id.textYongJin);
+        textFanYong = (TextView) findViewById(R.id.textFanYong);
     }
 
     @Override
@@ -47,24 +56,37 @@ public class WoDeSYActivity extends ZjbBaseActivity implements View.OnClickListe
     @Override
     protected void setListeners() {
         findViewById(R.id.imageBack).setOnClickListener(this);
-        findViewById(R.id.viewYongJinTX).setOnClickListener(this);
-        findViewById(R.id.viewFanXianTX).setOnClickListener(this);
+        findViewById(R.id.viewFenRun).setOnClickListener(this);
+        findViewById(R.id.viewYongJin).setOnClickListener(this);
+        findViewById(R.id.viewFanYong).setOnClickListener(this);
     }
 
     @Override
     protected void initData() {
-
+        SpannableString span = new SpannableString("¥" + "51513.00");
+        span.setSpan(new RelativeSizeSpan(0.5f), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textFenRun.setText(span);
+        SpannableString span1 = new SpannableString("¥" + "588.00");
+        span.setSpan(new RelativeSizeSpan(0.5f), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textYongJin.setText(span1);
+        SpannableString span2 = new SpannableString("¥" + "588.00");
+        span.setSpan(new RelativeSizeSpan(0.5f), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textFanYong.setText(span2);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
         switch (v.getId()) {
-            case R.id.viewYongJinTX:
+            case R.id.viewFenRun:
                 intent.setClass(this, TuiGuangYJActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.viewFanXianTX:
+            case R.id.viewYongJin:
+                intent.setClass(this, TuiGuangYJActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.viewFanYong:
                 intent.setClass(this, TuiGuangYJActivity.class);
                 startActivity(intent);
                 break;
