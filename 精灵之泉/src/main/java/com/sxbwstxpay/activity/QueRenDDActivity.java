d2c9c018f770145f74fe576dc12e89e3ac6740a5
ZhangJieBo <530092772@qq.com>
2017-09-24 17:42:59 +0800

    选择收货地址

---
+++ 精灵之泉/src/main/java/com/sxbwstxpay/activity/QueRenDDActivity.java
@@ -0,0 +1,128 @@
+package com.sxbwstxpay.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jlzquan.www.R;
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
+import com.sxbwstxpay.base.ZjbBaseActivity;
+import com.sxbwstxpay.util.ScreenUtils;
+import com.sxbwstxpay.viewholder.QueRenDDViewHolder;
+
+public class QueRenDDActivity extends ZjbBaseActivity implements View.OnClickListener {
+
+    private View viewBar;
+    private EasyRecyclerView recyclerView;
+    private RecyclerArrayAdapter<Integer> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_que_ren_dd);
+        init();
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void findID() {
+        viewBar = findViewById(R.id.viewBar);
+        recyclerView = (EasyRecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    @Override
+    protected void initViews() {
+        ((TextView) findViewById(R.id.textViewTitle)).setText("产品详情");
+        ViewGroup.LayoutParams layoutParams = viewBar.getLayoutParams();
+        layoutParams.height = (int) (getResources().getDimension(R.dimen.titleHeight) + ScreenUtils.getStatusBarHeight(this));
+        viewBar.setLayoutParams(layoutParams);
+        initRecycle();
+    }
+
+    @Override
+    protected void setListeners() {
+        findViewById(R.id.imageBack).setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+        onRefresh();
+    }
+
+    private void initRecycle() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        int red = getResources().getColor(R.color.basic_color);
+        recyclerView.setRefreshingColor(red);
+        recyclerView.getSwipeToRefresh().setProgressViewOffset(true, 30, 220);
+        recyclerView.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Integer>(this) {
+            @Override
+            public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                int layout = R.layout.item_queren_dd;
+                return new QueRenDDViewHolder(parent, layout);
+            }
+
+            @Override
+            public int getViewType(int position) {
+                Integer item = getItem(position);
+                return item;
+            }
+        });
+        adapter.setOnItemClickListener(new RecyclerArrayAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+        });
+        adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
+            @Override
+            public View onCreateView(ViewGroup parent) {
+                View view = LayoutInflater.from(QueRenDDActivity.this).inflate(R.layout.header_queren_dd, null);
+                view.findViewById(R.id.viewXuanZeSHDZ).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent();
+                        intent.setClass(QueRenDDActivity.this, XuanZeSHDZActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                return view;
+            }
+
+            @Override
+            public void onBindView(View headerView) {
+
+            }
+        });
+    }
+
+    public void onRefresh() {
+        adapter.clear();
+        adapter.add(1);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageBack:
+                finish();
+                break;
+        }
+    }
+}
--- 精灵之泉/src/main/java/com/sxbwstxpay/activity/XuanZeSHDZActivity.java
+++ 精灵之泉/src/main/java/com/sxbwstxpay/activity/XuanZeSHDZActivity.java
@@ -1,9 +1,8 @@
 package com.sxbwstxpay.activity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +16,19 @@ import com.jude.easyrecyclerview.decoration.DividerDecoration;
 import com.sxbwstxpay.base.ZjbBaseActivity;
 import com.sxbwstxpay.provider.DataProvider;
 import com.sxbwstxpay.util.ScreenUtils;
-import com.sxbwstxpay.viewholder.GouWuCViewHolder;
+import com.sxbwstxpay.viewholder.XuanZeSHDZViewHolder;
 
-public class GouWuCActivity extends ZjbBaseActivity implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
-    private RecyclerArrayAdapter<Integer> adapter;
+public class XuanZeSHDZActivity extends ZjbBaseActivity implements View.OnClickListener {
+
     private EasyRecyclerView recyclerView;
+    private RecyclerArrayAdapter<Integer> adapter;
+    private Handler handler = new Handler();
     private View viewBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gou_wu_c);
+        setContentView(R.layout.activity_xuan_ze_shdz);
         init();
     }
 
@@ -42,23 +44,22 @@ public class GouWuCActivity extends ZjbBaseActivity implements View.OnClickListe
 
     @Override
     protected void findID() {
-        recyclerView = (EasyRecyclerView) findViewById(R.id.recyclerView);
         viewBar = findViewById(R.id.viewBar);
+        recyclerView = (EasyRecyclerView) findViewById(R.id.recyclerView);
     }
 
     @Override
     protected void initViews() {
-        ((TextView) findViewById(R.id.textViewTitle)).setText("购物车");
+        ((TextView) findViewById(R.id.textViewTitle)).setText("选择收货地址");
         ViewGroup.LayoutParams layoutParams = viewBar.getLayoutParams();
         layoutParams.height = (int) (getResources().getDimension(R.dimen.titleHeight) + ScreenUtils.getStatusBarHeight(this));
         viewBar.setLayoutParams(layoutParams);
-        initRecycler();
+        initRecycle();
     }
 
     @Override
     protected void setListeners() {
         findViewById(R.id.imageBack).setOnClickListener(this);
-        findViewById(R.id.buttonJieSuan).setOnClickListener(this);
     }
 
     @Override
@@ -66,44 +67,51 @@ public class GouWuCActivity extends ZjbBaseActivity implements View.OnClickListe
         onRefresh();
     }
 
-    private void initRecycler() {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        DividerDecoration itemDecoration = new DividerDecoration(Color.TRANSPARENT, (int) getResources().getDimension(R.dimen.line_width), 0, 0);
+    private void initRecycle() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerDecoration itemDecoration = new DividerDecoration(Color.TRANSPARENT, (int) getResources().getDimension(R.dimen.marginTop), 0, 0);
         itemDecoration.setDrawLastItem(false);
         recyclerView.addItemDecoration(itemDecoration);
-        recyclerView.setRefreshingColor(getResources().getColor(R.color.basic_color));
-        recyclerView.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Integer>(GouWuCActivity.this) {
+        int red = getResources().getColor(R.color.basic_color);
+        recyclerView.setRefreshingColor(red);
+        recyclerView.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Integer>(this) {
             @Override
             public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
-                int layout = R.layout.item_car;
-                return new GouWuCViewHolder(parent, layout);
+                int layout = R.layout.item_xuanze_shdz;
+                return new XuanZeSHDZViewHolder(parent, layout);
+            }
+
+            @Override
+            public int getViewType(int position) {
+                Integer item = getItem(position);
+                return item;
             }
         });
         adapter.setOnItemClickListener(new RecyclerArrayAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
+
             }
         });
-        recyclerView.setRefreshListener(this);
+    }
+
+    public void onRefresh() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adapter.clear();
+                adapter.addAll(DataProvider.getPersonList(1));
+            }
+        }, 0);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent();
         switch (v.getId()) {
-            case R.id.buttonJieSuan:
-                intent.setClass(this,QueRenDDActivity.class);
-                startActivity(intent);
-                break;
             case R.id.imageBack:
                 finish();
                 break;
         }
-    }
-
-    @Override
-    public void onRefresh() {
-        adapter.clear();
-        adapter.addAll(DataProvider.getPersonList(1));
     }
 }
