10f68412e450f8d67c3bc20a185cc3c1d3018cb4
zhangjiebo <530092772@qq.com>
2018-02-02 09:50:39 +0800

    2018年2月2日09:50:32

--- 精灵之泉/src/main/java/com/sxbwstxpay/activity/YinDaoActivity.java
+++ 精灵之泉/src/main/java/com/sxbwstxpay/activity/YinDaoActivity.java
@@ -51,9 +51,7 @@ public class YinDaoActivity extends FragmentActivity {
 
         @Override
         public Fragment getItem(int position) {
-            GuideFragment guideFragment = new GuideFragment();
-            guideFragment.setImg(imgs[position], imgs.length - 1, position);
-            return guideFragment;
+            return new GuideFragment(imgs[position], imgs.length - 1, position);
         }
 
         @Override
